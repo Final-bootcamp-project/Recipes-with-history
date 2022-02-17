683c17ce5b0b8b84e23a5bcd90bc32a2f657a1cb
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-02-17 21:50:11 +0100

    changed API_URL

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,7 +1,8 @@
-export const API_URL = (slug) => `http://localhost:8090${slug}`;
+export const API_URL = (slug) =>
+	`https://final-project-jessica-rebecca.herokuapp.com/${slug}`;
 
 export const API_PROFILE = (userId) =>
-	`http://localhost:8090/profile/${userId}`;
+	`https://final-project-jessica-rebecca.herokuapp.com/profile/${userId}`;
 
 // export const LIKES_URL = (likeId) =>
 // 	`https://here-we-will-put-our-heroku-adress.herokuapp.com/${likeId}/like`;
